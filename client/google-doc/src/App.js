c1524fd036dd93b8551fde03586d0ac32b50a84f
Gaurav <iamGaurav201@gmail.com>
2023-07-03 02:47:05 +0530

    new commit

--- client/google-doc/src/App.js
+++ client/google-doc/src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
+import TextEditor from "./TextEditor";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hi</h1>
-    </div>
+    <TextEditor/>
   );
 }
 
